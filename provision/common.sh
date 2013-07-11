efbf5741b831a2b09827254c2f3ddba7ed166fc7
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-07-11 09:16:58 -0500

    a

---
+++ provision/common.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# 
+#
+LIB_AGENT="/var/lib/opscenter-agent/"
+AGENT_CONF="$LIB_AGENT/conf"
+AGENT_ADDR="$AGENT_CONF/address.yaml"
+
+#
+#
+#
+ETC_OPSC="/etc/opscenter"
+OPSC_CONF="$ETC_OPSC/opscenterd.conf"
+OPSC_CLUSTERS="$ETC_OPSC/clusters"
+
+
---
+++ provision/opsc-create.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# 
+#
+LIB_AGENT="/var/lib/opscenter-agent/"
+AGENT_CONF="$LIB_AGENT/conf"
+AGENT_ADDR="$AGENT_CONF/address.yaml"
+
