d4fcea010db8ef7e319ff4cad18d4363c9b9ad9c
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-07-22 19:33:00 -0500

    a

--- src/main/puppet/modules/cassandra_00/manifests/params.pp
+++ src/main/puppet/modules/cassandra_00/manifests/params.pp
@@ -146,8 +146,8 @@ class cassandra_00::params {
     }
     
     $rpc_address = $cassandra_00::rpc_address ? {
-        undef   => $ipaddress, # from facter
-#        undef   => "0.0.0.0", # need "all" for eqx
+#        undef   => $ipaddress, # from facter
+        undef   => "0.0.0.0", # need "all" for eqx
         default => $cassandra_00::rpc_address,
     }
 
